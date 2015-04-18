8e7d082b2f8733f6e5009605c665a5c5357f80ac
Kenny Lowe <kennethrlowe@gmail.com>
2015-04-18 00:23:34 -0700

    Adding weapon switching, hold to fire

--- src/js/game.js
+++ src/js/game.js
@@ -14,14 +14,34 @@ class Game {
       , y = this.game.height / 2;
 
     this.player = this.add.sprite(x, y, "player");
-    this.player.weapon = new BackAndForth(this, 'basic_bullet');
+    this.player.weapons = []
+    this.player.weapons.push(new StraightForward(this, 'basic_bullet'));
+    this.player.weapons.push(new Spread(this, 'basic_bullet'));
+    this.player.weapons.push(new BackAndForth(this, 'basic_bullet'));
+    this.player.weaponIndex = 0;
+    this.player.weapon = this.player.weapons[this.player.weaponIndex];
     this.player.anchor.setTo(0.5, 0.5);
-    this.input.onDown.add(this.onInputDown, this);
+    this.input.onHold.add(this.onInputDown, this);
   }
 
   update() {
+      
     this.player.position.x = this.input.position.x;
     this.player.position.y = this.input.position.y;
+      
+    if (this.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) { 
+      this.player.weapon.fire(this.player);
+    }
+      
+    if (this.input.keyboard.isDown(Phaser.Keyboard.CONTROL)) {
+      this.player.weaponIndex++;
+      if (this.player.weaponIndex >= this.player.weapons.length) {
+        this.player.weaponIndex = 0;
+      }
+      this.player.weapon = this.player.weapons[this.player.weaponIndex];
+      
+    }
+      
   }
 
   onInputDown() {
