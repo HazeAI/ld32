9ef4a4a920ec0263b0d5345f4b43eadb8d336392
Owen Nelson <onelson@gmail.com>
2015-04-18 21:46:05 -0700

    remove dead code

--- src/js/boot.js
+++ src/js/boot.js
@@ -12,10 +12,6 @@ class Boot {
     this.game.scale.pageAlignHorizontally = true;
     this.game.scale.pageAlignVertically = true;
     this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-    //this.game.scale.minWidth = this.game.width;
-    //this.game.scale.minHeight = this.game.height;
-    //this.game.scale.maxWidth = this.game.width;
-    //this.game.scale.maxHeight = this.game.height;
     this.game.scale.forceOrientation(true);
     this.game.scale.setScreenSize(true);
     this.game.state.start('preloader');
