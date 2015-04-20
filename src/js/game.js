b012c7b0816633f4c0b01ce8947bc6b0d9fec314
Owen Nelson <onelson@gmail.com>
2015-04-19 17:38:44 -0700

    monster closet is real

--- src/js/game.js
+++ src/js/game.js
@@ -9,12 +9,13 @@ TODO: Implement Score, Lives, Health
 */
 
 const Player = require('player');
+const enemy = require('enemy');
 const {
   Drone,
   RangedDrone,
   Chaser,
   RangedChaser
-} = require('enemy');
+} = enemy;
 const { randomize } = require('weapon');
 
 
@@ -28,13 +29,11 @@ class Game {
   create() {
     const x = this.game.width / 2;
     const y = this.game.height / 2;
-    
+
+    // this seems to be undone by the tilemap setup (resizeWorld).
     this.game.world.setBounds(0, 0, this.game.width, this.game.height);
 
-    this.map = this.game.add.tilemap('garbage');
-    this.map.addTilesetImage('tiles', 'gameTiles');
-    this.backgroundlayer = this.map.createLayer('testLayer');
-    this.backgroundlayer.resizeWorld();
+    this.setupTilemap();
       
     this.enemies = new Phaser.Group(this.game, this.game.world,
                                     'Enemies', false, true,
@@ -56,18 +55,41 @@ class Game {
     //////////////////////////////////////////////////
     
     this.rndWeapon();
-
     this.setupKeyboard();
     this.setupGamepad();
+  }
+
+  setupTilemap() {
+    this.map = this.game.add.tilemap('garbage');
+    this.map.addTilesetImage('tiles', 'gameTiles');
+    this.backgroundlayer = this.map.createLayer('testLayer');
+    this.backgroundlayer.resizeWorld();
+    this.spawns = this.map.objects.spawnPoints;
+  }
+
+  doSpawns() {
+    const toSpawn = this.spawns.filter(sp => !sp.empty && this.isOnCamera({body: {x: sp.x, y: sp.y}}));
+
+    toSpawn.forEach(function (spawn) {
+      for (let i = 0; i < spawn.properties.count; i++) {
+        const entity = new enemy[spawn.properties.entity]('eyeball', this);
+        this.enemies.add(entity);
+        entity.reset(
+          this.game.width + this.game.camera.x,
+          this.game.rnd.between(0, this.game.height)
+        );
+      }
+      spawn.empty = true;
+    }.bind(this));
   }
 
   update() {
     const dpad_rando_wep = 2;
 
     // value to adjust the delta by for the background scroll speed.
-    const scrollScaleFactor = 0.1;
+    const scrollScaleFactor = 0.25;
     this.camera.x += (this.time.elapsedMS * scrollScaleFactor);
-    
+    this.doSpawns();
     this.player.update();
     this.enemies.update();
     this.physics.arcade.overlap(this.player.weapon, this.enemies, this.enemyHit, null, this);
@@ -79,9 +101,9 @@ class Game {
                    this.game.rnd.between(0, this.game.height));
       }
     }, this);
-      
+
     if (this.player.lives <= 0) {
-      console.log('GAME OVER');   
+      console.log('GAME OVER');
     }
       
     this.scoreText.text = 'SCORE: '+this.player.score;
@@ -113,6 +135,7 @@ class Game {
   }
     
   isOnCamera(thing) {
+    // FIXME: should take x value as argument, not thing with a body that has an x on it.
     if (thing.body.x < this.camera.x-200 || 
         thing.body.x > this.camera.x+this.camera.width+200) {
       return false;   
